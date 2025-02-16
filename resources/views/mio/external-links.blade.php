16b27a1df63581a764465962846d19382788b1c3
JorellAndreiFinez <jorellandrei23@gmail.com>
2025-02-16 22:35:41 +0800

    HomePage

--- resources/views/mio/external-links.blade.php
+++ resources/views/mio/external-links.blade.php
@@ -5,10 +5,13 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 
 
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/alphardex/aqua.css/dist/aqua.min.css"> -->
 
 
 <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 
